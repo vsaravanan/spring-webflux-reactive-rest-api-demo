3ea9b8c44b58d2078530e79f232eef9cc42bb8e5
viswa <saravanan.resume@gmail.com>
2023-11-24 14:14:20 +0800

    jarname=SpringReactiveMulti

--- SpringReactiveFlyway.sh
+++ SpringReactiveFlyway.sh
@@ -2,6 +2,6 @@ rootdir=`pwd`
 jarname=SpringReactiveMulti
 rm -rf $rootdir/logs/*
 rm -rf $rootdir/logPath_IS_UNDEFINED
-rm -rf /data/logs/spring-reactive-multi
+rm -rf /data/logs/$jarname
 java -jar target/$jarname.jar
 
